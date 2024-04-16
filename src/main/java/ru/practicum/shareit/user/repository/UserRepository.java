65fad60762d211c949cd3216ce6b728b9291114d
Nikolay <Novizdarialokin@yandex.ru>
2024-04-16 18:13:16 +0600

    style: JavaDoc

--- src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
@@ -5,14 +5,48 @@ import ru.practicum.shareit.user.model.User;
 
 import java.util.List;
 
+/**
+ * Интерфейс {@link UserRepository}.
+ *
+ * @author Nikolay Radzivon.
+ */
 public interface UserRepository {
+    /**
+     * Метод добавления нового пользователя в репозиторий.
+     *
+     * @param user объект класса {@link User} для добавления.
+     * @return объект класса {@link UserDto}.
+     */
     UserDto save(User user);
 
+    /**
+     * Метод обновления пользователя по id в репозитории.
+     *
+     * @param user   объект класса {@link User} для обновления.
+     * @param userId идентификационный номер пользователя.
+     * @return объект класса {@link UserDto}.
+     */
     UserDto update(User user, long userId);
 
+    /**
+     * Метод получения пользователя по id из репозитория.
+     *
+     * @param userId идентификационный номер пользователя.
+     * @return объект класса {@link UserDto}.
+     */
     UserDto findById(long userId);
 
+    /**
+     * Метод удаления пользователя по id из репозитория.
+     *
+     * @param userId идентификационный номер пользователя.
+     */
     void deleteById(long userId);
 
+    /**
+     * Метод получения списка всех пользователей из репозитория.
+     *
+     * @return {@link List} объектов {@link UserDto}.
+     */
     List<UserDto> findAll();
 }
