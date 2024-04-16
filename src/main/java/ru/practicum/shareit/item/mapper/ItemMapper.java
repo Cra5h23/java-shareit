4d157d81d4781e95dcb93ee97ca396de184f512f
Nikolay <Novizdarialokin@yandex.ru>
2024-04-16 15:55:31 +0600

    style: Доки

--- src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -4,16 +4,19 @@ import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
 
 /**
- * Маппер для классов {@link ru.practicum.shareit.item.model.Item} и {@link ru.practicum.shareit.item.dto.ItemDto}
+ * Маппер для классов {@link ru.practicum.shareit.item.model.Item} и {@link ru.practicum.shareit.item.dto.ItemDto}.
+ * <p>
+ * Предназначен для преобразования объекта класса {@link Item} в объект класса {@link ItemDto} и наоборот.
  *
- * @author Nikolay Radzivon
+ * @author Nikolay Radzivon.
  */
 public class ItemMapper {
     /**
-     * Метод для преобразования объекта {@link Item} в объект {@link ItemDto}
+     * Метод для преобразования объекта {@link Item} в объект {@link ItemDto}.
+     *
      * @param item
      * @param id
-     * @return объект класса {@link ItemDto}
+     * @return объект класса {@link ItemDto}.
      */
     public ItemDto toItemDto(Item item, Long id) {
         return ItemDto.builder()
@@ -25,10 +28,11 @@ public class ItemMapper {
     }
 
     /**
-     * Метод для преобразования объекта класса {@link ItemDto} в объект класса {@link Item}
+     * Метод для преобразования объекта класса {@link ItemDto} в объект класса {@link Item}.
+     *
      * @param itemDto
      * @param userId
-     * @return объект класса {@link Item}
+     * @return объект класса {@link Item}.
      */
     public Item toItem(ItemDto itemDto, Long userId) {
         return Item.builder()
