aae6c0b99008afaf5cf70ed1859c07d1ea953b58
Nikolay <Novizdarialokin@yandex.ru>
2024-04-16 16:47:38 +0600

    style: Доки

--- src/main/java/ru/practicum/shareit/user/service/UserService.java
+++ src/main/java/ru/practicum/shareit/user/service/UserService.java
@@ -5,14 +5,43 @@ import ru.practicum.shareit.user.model.User;
 
 import java.util.List;
 
+/**
+ * Интерфейс {@link UserService}
+ *
+ * @author Nikolay Radzivon
+ */
 public interface UserService {
+    /**
+     * Метод добавления нового пользователя.
+     * @param user объект класса {@link User}.
+     * @return объект класса {@link UserDto}.
+     */
     UserDto addNewUser(User user);
 
+    /**
+     * Метод для обновления данных пользователей по id.
+     * @param user объект класса {@link User}.
+     * @param userId идентификационный номер пользователя.
+     * @return обновлённый объект класса {@link UserDto}.
+     */
     UserDto updateUser(User user, long userId);
 
+    /**
+     * Метод получения пользователя по его id.
+     * @param userId идентификационный номер пользователя.
+     * @return объект класса {@link UserDto}.
+     */
     UserDto getUser(long userId);
 
+    /**
+     * Метод удаления пользователя по его id.
+     * @param userId идентификационный номер пользователя.
+     */
     void deleteUser(long userId);
 
+    /**
+     * Метод получения списка всех пользователей.
+     * @return {@link List} объектов {@link UserDto}.
+     */
     List<UserDto> getAllUsers();
 }
