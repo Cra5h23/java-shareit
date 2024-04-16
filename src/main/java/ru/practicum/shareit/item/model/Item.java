1c87ee3904160e73d5ff60331717d5869a4b4337
Nikolay <Novizdarialokin@yandex.ru>
2024-04-16 14:50:46 +0600

    feat: Добавил поля класса Item

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,33 @@
 package ru.practicum.shareit.item.model;
 
+import lombok.Data;
+
 /**
- * TODO Sprint add-controllers.
+ * Класс вещь
+ *
+ * @author Nikolay Radzivon
  */
+
+@Data
 public class Item {
+    /**
+     * Уникальный идентификационный номер вещи.
+     */
+    private Long id;
+    /**
+     * Имя вещи.
+     */
+    private String name;
+    /**
+     * Описание вещи.
+     */
+    private String description;
+    /**
+     * Доступность вещи для аренды.
+     */
+    private Boolean available;
+    /**
+     * Идентификационный номер пользователя владельца вещи.
+     */
+    private Long userId;
 }
