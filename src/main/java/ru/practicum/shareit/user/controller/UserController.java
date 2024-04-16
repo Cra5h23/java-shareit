0035844a1522124d012d3c4d93eeee4005673af7
Nikolay <Novizdarialokin@yandex.ru>
2024-04-16 16:19:00 +0600

    feat: Добавил метод addNewUser для работы с эндпоинтом POST /users

--- src/main/java/ru/practicum/shareit/user/controller/UserController.java
+++ src/main/java/ru/practicum/shareit/user/controller/UserController.java
@@ -1,12 +1,30 @@
 package ru.practicum.shareit.user.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.service.UserService;
+
+import javax.validation.Valid;
 
 /**
- * TODO Sprint add-controllers.
+ * Контроллер для работы с эндпоинтами /users
+ *
+ * @author Nikolay Radzivon
  */
 @RestController
 @RequestMapping(path = "/users")
+@RequiredArgsConstructor
+@Slf4j
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDto addNewUser(@Valid @RequestBody User user) {
+        return userService.addNewUser(user);
+    }
 }
