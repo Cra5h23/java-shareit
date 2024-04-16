22aa4c228217cf55d3dc1b59a4df37390b3475d5
Nikolay <Novizdarialokin@yandex.ru>
2024-04-16 17:01:02 +0600

    feat: Добавил интерфейс UserRepository

---
+++ src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package ru.practicum.shareit.user.repository;
+
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.model.User;
+
+public interface UserRepository {
+    UserDto save(User user);
+
+}
