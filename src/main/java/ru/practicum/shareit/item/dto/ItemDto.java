b925cd9f5789bff656c863f4d0f9e7f5ea418882
Nikolay <Novizdarialokin@yandex.ru>
2024-04-16 15:04:55 +0600

    feat: Добавил поля класса ItemDto

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,32 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
- * TODO Sprint add-controllers.
+ * @author Nikolay Radzivon
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
 public class ItemDto {
+    /**
+     * Уникальный идентификационный номер вещи.
+     */
+    private Long id;
+    /**
+     * Название вещи.
+     */
+    private String name;
+    /**
+     * Описание вещи.
+     */
+    private String description;
+    /**
+     * Доступность вещи для аренды.
+     */
+    private Boolean available;
 }
