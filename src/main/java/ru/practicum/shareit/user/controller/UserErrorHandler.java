0dd0cabdb891109fabf8212136d2420f26c2d372
Nikolay <Novizdarialokin@yandex.ru>
2024-04-17 12:50:29 +0600

    feat: Добавил метод handlerUserRepositoryException для UserRepositoryException

--- src/main/java/ru/practicum/shareit/user/controller/UserErrorHandler.java
+++ src/main/java/ru/practicum/shareit/user/controller/UserErrorHandler.java
@@ -1,7 +1,18 @@
 package ru.practicum.shareit.user.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import ru.practicum.shareit.user.exeption.UserRepositoryException;
+import ru.practicum.shareit.user.exeption.UserServiceException;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author Nikolay Radzivon
@@ -10,4 +21,14 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 @ControllerAdvice("ru.practicum.shareit.user")
 @Slf4j
 public class UserErrorHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<?> handlerUserRepositoryException(final UserRepositoryException e) {
+        log.warn("Ошибка работы с пользователями", e);
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(Map.of("timestamp", LocalDateTime.now(),
+                        "status", HttpStatus.CONFLICT.value(),
+                        "Ошибка работы с пользователями", e.getMessage()));
+    }
 }
