49c14257dca9cd42cf1e6b1295d1b873e15b269a
Nikolay <Novizdarialokin@yandex.ru>
2024-04-17 13:13:30 +0600

    feat: Добавил метод addNewItem

--- src/main/java/ru/practicum/shareit/item/service/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemService.java
@@ -1,8 +1,21 @@
 package ru.practicum.shareit.item.service;
 
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
+
 /**
+ * Интерфейс {@link ItemService}
+ *
  * @author Nikolay Radzivon
  * @Date 17.04.2024
  */
 public interface ItemService {
+    /**
+     * Метод добавления нового предмета.
+     *
+     * @param item   объект класса {@link Item}.
+     * @param userId идентификационный номер пользователя владельца вещи.
+     * @return объект класса {@link ItemDto}.
+     */
+    ItemDto addNewItem(Item item, Long userId);
 }
