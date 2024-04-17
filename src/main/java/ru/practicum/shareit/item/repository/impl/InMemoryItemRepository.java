c97576982eae3fdb4dc530e99d19d2620eb90ef6
Nikolay <Novizdarialokin@yandex.ru>
2024-04-17 19:33:01 +0600

    feat: Реализовал метод save

--- src/main/java/ru/practicum/shareit/item/repository/impl/InMemoryItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/impl/InMemoryItemRepository.java
@@ -1,17 +1,29 @@
 package ru.practicum.shareit.item.repository.impl;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.mapper.ItemMapper;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.repository.ItemRepository;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 /**
+ * Реализация интерфейса {@link ItemRepository} для хранения предметов в памяти приложения.
+ *
  * @author Nikolay Radzivon
  * @Date 17.04.2024
  */
+@Repository
+@Slf4j
+@RequiredArgsConstructor
 public class InMemoryItemRepository implements ItemRepository {
+    private final Map<Long, List<Item>> itemMap = new HashMap<>();
+    private Long generatorId = 0L;
+    private final ItemMapper itemMapper;
+
     /**
      * Метод добавления нового предмета в репозиторий.
      *
@@ -21,7 +33,14 @@ public class InMemoryItemRepository implements ItemRepository {
      */
     @Override
     public ItemDto save(Item item, Long userId) {
-        return null;
+        List<Item> items = itemMap.getOrDefault(userId, new ArrayList<>());
+        item.setId(++generatorId);
+        if (!items.contains(item)) {
+            items.add(item);
+        }
+        itemMap.put(userId, items);
+        log.info("В репозиторий для пользователя {} добавлен новый предмет {} и присвоен id {}", userId, item, item.getId());
+        return itemMapper.toItemDto(item);
     }
 
     /**
