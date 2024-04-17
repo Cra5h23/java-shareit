4605b637af1dad9f44e6b2ed3237cf935e9b6784
Nikolay <Novizdarialokin@yandex.ru>
2024-04-17 19:32:24 +0600

    feat: Теперь компонент

--- src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.item.mapper;
 
+import org.springframework.stereotype.Component;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
 
@@ -10,17 +11,17 @@ import ru.practicum.shareit.item.model.Item;
  *
  * @author Nikolay Radzivon.
  */
+@Component
 public class ItemMapper {
     /**
      * Метод для преобразования объекта {@link Item} в объект {@link ItemDto}.
      *
      * @param item
-     * @param id
      * @return объект класса {@link ItemDto}.
      */
-    public ItemDto toItemDto(Item item, Long id) {
+    public ItemDto toItemDto(Item item) {
         return ItemDto.builder()
-                .id(id)
+                .id(item.getId())
                 .name(item.getName())
                 .description(item.getDescription())
                 .available(item.getAvailable())
