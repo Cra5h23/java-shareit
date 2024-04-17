9a06fcbd8bd2bdb5f60a293ef1b0814dc6ea3078
Nikolay <Novizdarialokin@yandex.ru>
2024-04-17 11:21:05 +0600

    feat: Класс UserErrorHandler для обработки исключений при работе с пользователями и возвращения форматированного ответа

---
+++ src/main/java/ru/practicum/shareit/user/controller/UserErrorHandler.java
@@ -0,0 +1,13 @@
+package ru.practicum.shareit.user.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 17.04.2024
+ */
+@ControllerAdvice("ru.practicum.shareit.user")
+@Slf4j
+public class UserErrorHandler {
+}
