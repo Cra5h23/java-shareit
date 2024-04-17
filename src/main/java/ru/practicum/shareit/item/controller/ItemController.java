d9725eb4927e5d9572c2384b185698de04f5e02e
Nikolay <Novizdarialokin@yandex.ru>
2024-04-17 13:13:16 +0600

    feat: Реализовал метод addNewItem

--- src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -1,12 +1,32 @@
 package ru.practicum.shareit.item.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.item.service.ItemService;
+
+import javax.validation.Valid;
 
 /**
- * TODO Sprint add-controllers.
+ * Контроллер для работы с эндпоинтами /items
+ *
+ * @author Nikolay Radzivon
+ * @Date 17.04.2024
  */
 @RestController
 @RequestMapping("/items")
+@RequiredArgsConstructor
+@Slf4j
 public class ItemController {
+    private final ItemService itemService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ItemDto addNewItem(@Valid @RequestBody Item item, @RequestHeader("X-Sharer-User-Id") Long userId) {
+        log.info("POST /items body = {}, header \"X-Sharer-User-Id\" = {}", item, userId);
+        return itemService.addNewItem(item, userId);
+    }
 }
