3c5ff66b376ad79b6299e61b033ccb86e95acac2
Nikolay <Novizdarialokin@yandex.ru>
2024-04-17 15:08:52 +0600

    feat: добавил метод updateItem

--- src/main/java/ru/practicum/shareit/item/service/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemService.java
@@ -18,4 +18,15 @@ public interface ItemService {
      * @return объект класса {@link ItemDto}.
      */
     ItemDto addNewItem(Item item, Long userId);
+
+    /**
+     * Метод обновления предмета.
+     *
+     * @param item   объект класса {@link Item}.
+     * @param userId идентификационный номер пользователя владельца предмета.
+     * @param itemId идентификационный номер предмета.
+     * @return объект класса {@link ItemDto}
+     */
+    ItemDto updateItem(Item item, Long userId, Long itemId);
+
 }
