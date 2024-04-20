241352f1a70010bac34033061c02ed036d72be14
Nikolay <Novizdarialokin@yandex.ru>
2024-04-20 19:35:36 +0600

    feat: Добавил заготовку для тестов ItemControllerTest

---
+++ src/test/java/ru/practicum/shareit/item/controller/ItemControllerTest.java
@@ -0,0 +1,40 @@
+package ru.practicum.shareit.item.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 20.04.2024
+ */
+class ItemControllerTest {
+
+    @Test
+    void addNewItem() {
+    }
+
+    @Test
+    void updateItem() {
+    }
+
+    @Test
+    void getItemById() {
+    }
+
+    @Test
+    void deleteItemByItemId() {
+    }
+
+    @Test
+    void deleteAllItemByUser() {
+    }
+
+    @Test
+    void getAllItemByUser() {
+    }
+
+    @Test
+    void searchItemByText() {
+    }
+}
