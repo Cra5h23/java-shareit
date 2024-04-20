7b003e08816eb4f594a5b1554841daf01696ea58
Nikolay <Novizdarialokin@yandex.ru>
2024-04-20 19:29:19 +0600

    feat: Добавил ItemRequestDto

---
+++ src/main/java/ru/practicum/shareit/item/dto/ItemRequestDto.java
@@ -0,0 +1,38 @@
+package ru.practicum.shareit.item.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.Marker;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+/**
+ * Модель для принятия данных вещи из запроса.
+ *
+ * @author Nikolay Radzivon
+ * @Date 20.04.2024
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class ItemRequestDto {
+    /**
+     * Название вещи.
+     */
+    @NotBlank(groups = Marker.OnCreate.class, message = "Название предмета не может быть пустым")
+    private String name;
+    /**
+     * Описание вещи.
+     */
+    @NotBlank(groups = Marker.OnCreate.class, message = "Описание предмета не может быть пустым")
+    private String description;
+    /**
+     * Возможность аренды вещи.
+     */
+    @NotNull(groups = Marker.OnCreate.class, message = "Возможность аренды должна быть указана")
+    private Boolean available;
+}
