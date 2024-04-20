48ce61eb36d76ed9f4f820b688fc6545169e629d
Nikolay <Novizdarialokin@yandex.ru>
2024-04-20 19:32:34 +0600

    refactor:  Переделал в связи с добавлением класса ErrorResponse

--- src/main/java/ru/practicum/shareit/user/controller/UserErrorHandler.java
+++ src/main/java/ru/practicum/shareit/user/controller/UserErrorHandler.java
@@ -1,21 +1,22 @@
 package ru.practicum.shareit.user.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import ru.practicum.shareit.user.exeption.UserRepositoryException;
 import ru.practicum.shareit.user.exeption.UserServiceException;
 
-import java.time.LocalDateTime;
-import java.util.Map;
-import java.util.stream.Collectors;
+import javax.validation.ConstraintViolationException;
+
+import static ru.practicum.shareit.ErrorResponse.*;
 
 /**
- * Класс {@link UserErrorHandler} для обработки исключений {@link UserRepositoryException}, {@link UserServiceException}, {@link MethodArgumentNotValidException} в пакете
+ * Класс {@link UserErrorHandler} для обработки исключений {@link UserRepositoryException}, {@link UserServiceException},
+ * {@link MethodArgumentNotValidException}, {@link ConstraintViolationException} в пакете {@link ru.practicum.shareit.user}
  *
  * @author Nikolay Radzivon
  * @Date 17.04.2024
@@ -25,34 +26,27 @@ import java.util.stream.Collectors;
 public class UserErrorHandler {
 
     @ExceptionHandler
-    public ResponseEntity<?> handlerUserRepositoryException(final UserRepositoryException e) {
+    public ResponseEntity<?> handlerUserRepositoryException(final UserRepositoryException e, WebRequest webRequest) {
         log.warn("Ошибка работы с пользователями", e);
-        return ResponseEntity
-                .status(HttpStatus.CONFLICT)
-                .body(Map.of("timestamp", LocalDateTime.now(),
-                        "status", HttpStatus.CONFLICT.value(),
-                        "Ошибка работы с пользователями", e.getMessage()));
+        return makeErrorResponse(webRequest, HttpStatus.CONFLICT, "Ошибка работы с пользователями");
     }
 
     @ExceptionHandler
-    public ResponseEntity<?> handlerUserServiceException(final UserServiceException e) {
+    public ResponseEntity<?> handlerUserServiceException(final UserServiceException e, WebRequest webRequest) {
         log.warn("Ошибка работы с пользователями", e);
-        return ResponseEntity
-                .status(HttpStatus.NO_CONTENT)
-                .body(Map.of("timestamp", LocalDateTime.now(),
-                        "status", HttpStatus.NO_CONTENT.value(),
-                        "Ошибка работы с пользователями", e.getMessage()));
+        return makeErrorResponse(webRequest, HttpStatus.NO_CONTENT, "Ошибка работы с пользователями");
     }
 
     @ExceptionHandler
-    public ResponseEntity<?> handlerMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+    public ResponseEntity<?> handlerMethodArgumentNotValidException(final MethodArgumentNotValidException e,
+                                                                    WebRequest webRequest) {
         log.warn("Ошибка ввода данных пользователя", e);
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(Map.of("timestamp", LocalDateTime.now(),
-                        "status", HttpStatus.BAD_REQUEST.value(),
-                        "Ошибка ввода данных пользователя", e.getFieldErrors().stream()
-                                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                                .collect(Collectors.toList())));
+        return makeErrorResponse(webRequest, HttpStatus.BAD_REQUEST, "Ошибка ввода данных пользователя");
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<?> handlerConstraintViolationException(final ConstraintViolationException e, WebRequest webRequest) {
+        log.warn("Ошибка ввода данных пользователя", e);
+        return makeErrorResponse(webRequest, HttpStatus.BAD_REQUEST, "Ошибка ввода данных пользователя");
     }
 }
