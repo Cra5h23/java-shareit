3630590b54652ff6aaa2a16801e5849fdd27aa3b
Nikolay <Novizdarialokin@yandex.ru>
2024-04-20 19:26:19 +0600

    feat: Добавил метод handlerItemRepositoryException для обработки исключений ItemRepositoryException

--- src/main/java/ru/practicum/shareit/item/controller/ItemErrorHandler.java
+++ src/main/java/ru/practicum/shareit/item/controller/ItemErrorHandler.java
@@ -1,9 +1,29 @@
 package ru.practicum.shareit.item.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import ru.practicum.shareit.item.exception.ItemRepositoryException;
+import static ru.practicum.shareit.ErrorResponse.*;
 /**
+ * Класс {@link ItemErrorHandler} для обработки исключений {@link ItemRepositoryException}, {@link ItemServiceException},
+ * {@link MissingRequestHeaderException}, {@link ConstraintViolationException} в пакете {@link ru.practicum.shareit.item}
+ *
  * @author Nikolay Radzivon
  * @Date 19.04.2024
  */
+@ControllerAdvice("ru.practicum.shareit.item")
+@Slf4j
+@RequiredArgsConstructor
 public class ItemErrorHandler {
+    @ExceptionHandler
+    public ResponseEntity<?> handlerItemRepositoryException(final ItemRepositoryException e, WebRequest webRequest) {
+        log.warn("Ошибка работы с предметами", e);
+        return makeErrorResponse(webRequest, HttpStatus.NOT_FOUND, "Ошибка работы с предметами");
+    }
 
 }
