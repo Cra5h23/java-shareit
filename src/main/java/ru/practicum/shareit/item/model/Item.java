7ef92ccb50d77344964d9ec8f2c793797958e079
Nikolay <Novizdarialokin@yandex.ru>
2024-04-20 19:23:47 +0600

    feat: Добавил группы валидации

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.item.model;
 
 import lombok.*;
+import ru.practicum.shareit.Marker;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -24,20 +25,22 @@ public class Item {
     /**
      * Имя вещи.
      */
-    @NotBlank(message = "Название вещи не может быть пустым")
+    @NotBlank(groups = Marker.OnCreate.class, message = "Название вещи не может быть пустым")
     private String name;
     /**
      * Описание вещи.
      */
+    @NotBlank(groups = Marker.OnCreate.class, message = "Описание предмета не может быть пустым")
     private String description;
     /**
      * Доступность вещи для аренды.
      */
+    @NotNull(groups = Marker.OnCreate.class, message = "Возможность аренды должна быть указана")
     private Boolean available;
     /**
      * Идентификационный номер пользователя владельца вещи.
      */
-    @NotNull
+    @NotNull(groups = Marker.OnCreate.class, message = "Пользователь владелец предмета должен быть указан")
     @Builder.Default
     private Long userId = 0L;
 }
