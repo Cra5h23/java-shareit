8e8843950bf2533b9dd8a73bdd826e50f2be0a79
Nikolay <Novizdarialokin@yandex.ru>
2024-04-21 16:58:25 +0600

    feat: Добавил Swagger

--- src/main/java/ru/practicum/shareit/item/dto/ItemResponseDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemResponseDto.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.item.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,21 +15,26 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder(toBuilder = true)
+@Schema(name = "Вещь для ответа", description = "Сущность вещи для ответа")
 public class ItemResponseDto {
     /**
      * Уникальный идентификационный номер вещи.
      */
+    @Schema(description = "Идентификационный номер вещи", example = "1")
     private Long id;
     /**
      * Название вещи.
      */
+    @Schema(description = "Название вещи",example = "exampleName")
     private String name;
     /**
      * Описание вещи.
      */
+    @Schema(description = "Описание вещи", example = "exampleDescription")
     private String description;
     /**
      * Доступность вещи для аренды.
      */
+    @Schema(description = "Доступность для аренды", example = "true")
     private Boolean available;
 }
--- src/main/java/ru/practicum/shareit/user/dto/UserResponseDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserResponseDto.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,17 +15,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
+@Schema(name = "Пользователь для ответа", description = "Данные пользователя для ответа")
 public class UserResponseDto {
     /**
      * Идентификационный номер пользователя.
      */
+    @Schema(description = "Идентификационный номер пользователя", example = "1")
     private Long id;
     /**
      * Имя пользователя.
      */
+    @Schema(description = "Имя пользователя", example = "exampleUserName")
     private String name;
     /**
      * Электронная почта пользователя.
      */
+    @Schema(description = "Электронная почта пользователя", example = "exampleEmail@example.com")
     private String email;
 }
