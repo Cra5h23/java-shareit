1f13ef6567abc99f522bd7bb20896580d445c25f
Nikolay <Novizdarialokin@yandex.ru>
2024-04-21 09:32:10 +0600

    feat: Добавил заготовку для тестов UserControllerTest

---
+++ src/test/java/ru/practicum/shareit/user/controller/UserControllerTest.java
@@ -0,0 +1,32 @@
+package ru.practicum.shareit.user.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 21.04.2024
+ */
+class UserControllerTest {
+
+    @Test
+    void addNewUser() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+}
