e82baa7df1de05f9ff07c952e92b9771e14eb29e
Nikolay <Novizdarialokin@yandex.ru>
2024-04-21 12:34:00 +0600

    feat: Добавил новый клас для приёма данных пользователя из запроса.

---
+++ src/main/java/ru/practicum/shareit/user/dto/UserRequestDto.java
@@ -0,0 +1,32 @@
+package ru.practicum.shareit.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.Marker;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 21.04.2024
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class UserRequestDto {
+    /**
+     * Имя пользователя.
+     */
+    @NotBlank(groups = Marker.OnCreate.class, message = "Поле name не должно быть пустым")
+    private String name;
+    /**
+     * Электронная почта пользователя
+     */
+    @Email(groups = {Marker.OnCreate.class, Marker.OnUpdate.class}, message = "Поле email должно иметь формат адреса электронной почты")
+    @NotBlank(groups = {Marker.OnCreate.class}, message = "Поле email не должно быть пустым")
+    private String email;
+}
