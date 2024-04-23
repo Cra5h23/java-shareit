d476dea24d7c9eec9dba6d4123d1d3cbd984b7b0
Nikolay <Novizdarialokin@yandex.ru>
2024-04-23 15:09:19 +0600

    refactor: Переделал способ хранения пользователей List -> HashMap. Метод update больше не нужен теперь для обновления используется метод save

--- src/main/java/ru/practicum/shareit/user/repository/impl/InMemoryUserRepository.java
+++ src/main/java/ru/practicum/shareit/user/repository/impl/InMemoryUserRepository.java
@@ -5,12 +5,10 @@ import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.user.dto.UserRequestDto;
 import ru.practicum.shareit.user.dto.UserResponseDto;
 import ru.practicum.shareit.user.exeption.UserRepositoryException;
-import ru.practicum.shareit.user.mapper.UserMapper;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.repository.UserRepository;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * Реализация интерфейса {@link UserRepository} для хранения пользователей в памяти компьютера.
@@ -21,72 +19,45 @@ import java.util.stream.Collectors;
 @Slf4j
 public class InMemoryUserRepository implements UserRepository {
     private Long generatorId = 0L;
-    private final List<User> userList = new ArrayList<>();
+    private final Map<Long, User> userMap = new HashMap<>();
 
     /**
-     * Метод добавления нового пользователя в репозиторий.
+     * Метод добавления нового пользователя в репозиторий или обновления существующего.
      *
      * @param user объект класса {@link User} для добавления.
      * @return объект класса {@link UserResponseDto}.
      */
     @Override
-    public UserResponseDto save(UserRequestDto user) {
-        checkEmail(user, String.format(
-                "Нельзя добавить нового пользователя, Пользователь с email %s уже существует", user.getEmail()));
-
-        var id = ++generatorId;
-        User u = UserMapper.toUser(user, id);
-
-        userList.add(u);
-        log.info("В репозиторий добавлен новый пользователь {} и присвоен id {}", u, id);
-        return UserMapper.toUserResponseDto(u);
-    }
-
-    /**
-     * Метод обновления пользователя по id в репозитории.
-     *
-     * @param user   объект класса {@link User} для обновления.
-     * @param userId идентификационный номер пользователя.
-     * @return объект класса {@link UserResponseDto}.
-     */
-    @Override
-    public UserResponseDto update(UserRequestDto user, long userId) {
-        User updateUser = userList.stream()
-                .filter(u -> u.getId().equals(userId))
-                .findFirst()
-                .orElseThrow(() -> new UserRepositoryException(
-                        String.format("Не существует пользователя с id %d", userId)));
-
-        log.info("Обновлён пользователь с id {} , старые данные {} новые данные {}", userId, updateUser, user);
-
-        if (user.getEmail() != null) {
-            if (!updateUser.getEmail().equals(user.getEmail())) {
-                checkEmail(user, String.format(
-                        "Нельзя обновить пользователя с id %d, пользователь с email %s уже существует", userId, user.getEmail()));
-                updateUser.setEmail(user.getEmail());
-            }
+    public User save(final User user) {
+        if (user.getId() != null && userMap.containsKey(user.getId())) {
+            checkEmail(user, "обновить");
+            userMap.put(user.getId(), user);
+        } else {
+            checkEmail(user, "добавить");
+            var id = ++generatorId;
+            user.setId(id);
+            userMap.put(id, user);
         }
 
-        if (user.getName() != null) {
-            updateUser.setName(user.getName());
-        }
-
-        return UserMapper.toUserResponseDto(updateUser);
+        return user.toBuilder().build();
     }
 
     /**
      * Метод получения пользователя по id из репозитория.
      *
      * @param userId идентификационный номер пользователя.
-     * @return объект класса {@link UserResponseDto}.
+     * @return объект класса {@link User}.
      */
     @Override
-    public Optional<UserResponseDto> findById(long userId) {
+    public Optional<User> findById(long userId) {
         log.info("Запрошен пользователь с id {}", userId);
-        return userList.stream()
-                .filter(user -> user.getId().equals(userId))
-                .map(UserMapper::toUserResponseDto)
-                .findFirst();
+        User user = userMap.get(userId);
+
+        if (user == null) {
+            return Optional.empty();
+        }
+
+        return Optional.of(user.toBuilder().build());
     }
 
     /**
@@ -96,32 +67,35 @@ public class InMemoryUserRepository implements UserRepository {
      */
     @Override
     public void deleteById(long userId) {
-        userList.stream().filter(user -> user.getId().equals(userId)).findFirst().map(userList::remove);
         log.info("Удалён пользователь с id {}", userId);
+        userMap.remove(userId);
     }
 
     /**
      * Метод получения списка всех пользователей из репозитория.
      *
-     * @return {@link List} объектов {@link UserResponseDto}.
+     * @return {@link Collections} объектов {@link User}.
      */
     @Override
-    public List<UserResponseDto> findAll() {
+    public Collection<User> findAll() {
         log.info("Запрошен список всех пользователей");
-        return userList.stream().map(UserMapper::toUserResponseDto).collect(Collectors.toList());
+        return Collections.unmodifiableCollection(userMap.values());
     }
 
     /**
      * Метод проверки, что email не зарегистрирован.
-     * @param user объект класса {@link UserRequestDto}.
+     *
+     * @param user    объект класса {@link UserRequestDto}.
      * @param message Сообщение ошибки.
      */
-    private void checkEmail(UserRequestDto user, String message) {
-        Optional<User> first = userList.stream()
-                .filter(u -> u.getEmail().equals(user.getEmail()))
+    private void checkEmail(User user, String message) {
+        var s = "Нельзя %s нового пользователя, Пользователь с email %s уже существует";
+
+        Optional<User> first = userMap.values().stream()
+                .filter(u -> u.getEmail().equals(user.getEmail()) && !u.getId().equals(user.getId()))
                 .findFirst();
         if (first.isPresent()) {
-            throw new UserRepositoryException(message);
+            throw new UserRepositoryException(String.format(s, message, user.getEmail()));
         }
     }
 }
