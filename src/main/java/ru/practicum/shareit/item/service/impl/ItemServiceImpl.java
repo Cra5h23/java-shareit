2cb2c6faa6a9e944dc26aa5e03a33237ec29cfbd
Nikolay <Novizdarialokin@yandex.ru>
2024-04-23 15:23:15 +0600

    refactor: Теперь маппинг происходит в сервисе.

--- src/main/java/ru/practicum/shareit/item/service/impl/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/service/impl/ItemServiceImpl.java
@@ -6,12 +6,16 @@ import org.springframework.stereotype.Service;
 import ru.practicum.shareit.item.dto.ItemRequestDto;
 import ru.practicum.shareit.item.dto.ItemResponseDto;
 import ru.practicum.shareit.item.exception.ItemServiceException;
+import ru.practicum.shareit.item.mapper.ItemMapper;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.repository.ItemRepository;
 import ru.practicum.shareit.item.service.ItemService;
+import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.repository.UserRepository;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * Реализация интерфейса {@link ItemService}
@@ -26,26 +30,28 @@ public class ItemServiceImpl implements ItemService {
     private final ItemRepository itemRepository;
     private final UserRepository userRepository;
     private final String checkUserErrorMessage = "Нельзя %s вещ%s для не существующего пользователя с id %d";
-    private final String checkItemErrorMessage = "Вещь с id %d не существует%s";
 
     /**
      * Метод добавления новой вещи.
      *
-     * @param item   объект класса {@link Item}.
+     * @param item   объект класса {@link ItemRequestDto}.
      * @param userId идентификационный номер пользователя владельца вещи.
      * @return объект класса {@link ItemResponseDto}.
      */
     @Override
     public ItemResponseDto addNewItem(ItemRequestDto item, Long userId) {
-        checkUser(userId, String.format(checkUserErrorMessage, "создать новую", "ь", userId));
-        log.info("Создание новой вещи {} для пользователя с id {}", item, userId);
-        return itemRepository.save(item, userId);
+        User user = checkUser(userId, String.format(checkUserErrorMessage, "создать новую", "ь", userId));
+        Item i = ItemMapper.toItem(item, user);
+        Item save = itemRepository.save(i);
+
+        log.info("Создана новая вещь {} для пользователя с id {} и присвоен id {}", item, userId, save.getId());
+        return ItemMapper.toItemResponseDto(i);
     }
 
     /**
      * Метод обновления вещи.
      *
-     * @param item   объект класса {@link Item}.
+     * @param item   объект класса {@link ItemRequestDto}.
      * @param userId идентификационный номер пользователя владельца вещи.
      * @param itemId идентификационный номер вещи.
      * @return объект класса {@link ItemResponseDto}
@@ -53,8 +59,30 @@ public class ItemServiceImpl implements ItemService {
     @Override
     public ItemResponseDto updateItem(ItemRequestDto item, Long userId, Long itemId) {
         checkUser(userId, String.format(checkUserErrorMessage, "обновить", "ь", userId));
-        log.info("Обновление вещи c id {} для пользователя с id {}, новые данные {}", itemId, userId, item);
-        return itemRepository.update(item, userId, itemId);
+
+        Item i = checkItem(itemId, userId);
+        String name = item.getName();
+
+        log.info("Обновление вещи c id {} для пользователя с id {}, старые данные новые {} данные {}", itemId, userId, i, item);
+        if (name != null) {
+            i.setName(name);
+        }
+
+        Boolean available = item.getAvailable();
+
+        if (available != null) {
+            i.setAvailable(available);
+        }
+
+        String description = item.getDescription();
+
+        if (description != null) {
+            i.setDescription(description);
+        }
+
+        Item save = itemRepository.save(i);
+
+        return ItemMapper.toItemResponseDto(save);
     }
 
     /**
@@ -67,7 +95,8 @@ public class ItemServiceImpl implements ItemService {
     @Override
     public ItemResponseDto getItemByItemId(Long itemId, Long userId) {
         log.info("Получение вещи с id {}", itemId);
-        return checkItem(itemId, userId, String.format(checkItemErrorMessage, itemId, ""));
+        Item i = checkItem(itemId, null);
+        return ItemMapper.toItemResponseDto(i);
     }
 
     /**
@@ -79,10 +108,9 @@ public class ItemServiceImpl implements ItemService {
     @Override
     public void deleteItemByItemId(Long itemId, Long userId) {
         checkUser(userId, String.format(checkUserErrorMessage, "удалить", "ь c id " + itemId, userId));
-        checkItem(itemId, userId, String.format(
-                checkItemErrorMessage, itemId, String.format(" у пользователя с id %d", userId)));
+        checkItem(itemId, userId);
         log.info("Удаление вещи с id {} для пользователя с id {}", itemId, userId);
-        itemRepository.deleteById(itemId, userId);
+        itemRepository.deleteById(itemId);
     }
 
     /**
@@ -95,7 +123,8 @@ public class ItemServiceImpl implements ItemService {
     public List<ItemResponseDto> getAllItemByUser(Long userId) {
         checkUser(userId, String.format(checkUserErrorMessage, "получить список", "ей", userId));
         log.info("Получение списка всех вещей для пользователя с id {}", userId);
-        return itemRepository.findAllById(userId);
+        List<Item> allById = itemRepository.findAllById(userId);
+        return allById.stream().map(ItemMapper::toItemResponseDto).collect(Collectors.toList());
     }
 
     /**
@@ -107,10 +136,17 @@ public class ItemServiceImpl implements ItemService {
      */
     @Override
     public List<ItemResponseDto> searchItemByText(String text, Long userId) {
+        if (text.isBlank()) {
+            return List.of();
+        }
+
         checkUser(userId, String.format(
                 checkUserErrorMessage, "найти список", "ей с параметром поиска" + text, userId));
         log.info("Поиск вещей для пользователя с id {} с параметром поиска {}", userId, text);
-        return text != null ? itemRepository.search(text, userId) : List.of();
+
+        List<Item> search = itemRepository.search(text);
+
+        return search.stream().map(ItemMapper::toItemResponseDto).collect(Collectors.toList());
     }
 
     /**
@@ -131,19 +167,34 @@ public class ItemServiceImpl implements ItemService {
      * @param userId  идентификатор пользователя владельца вещи.
      * @param message текст сообщения ошибки.
      */
-    private void checkUser(Long userId, String message) {
-        userRepository.findById(userId).orElseThrow(() -> new ItemServiceException(message));
+    private User checkUser(Long userId, String message) {
+        return userRepository.findById(userId).orElseThrow(() -> new ItemServiceException(message));
     }
 
     /**
      * Метод проверки, что вещь существует.
      *
-     * @param itemId  идентификатор вещи.
-     * @param userId  идентификатор пользователя владельца вещи.
-     * @param message текст сообщения ошибки.
-     * @return
+     * @param itemId идентификатор вещи.
+     * @param userId идентификатор пользователя владельца вещи.
+     * @return объект класса {@link Item}
      */
-    private ItemResponseDto checkItem(Long itemId, Long userId, String message) {
-        return itemRepository.findById(itemId, userId).orElseThrow(() -> new ItemServiceException(message));
+    private Item checkItem(Long itemId, Long userId) {
+        var s = "Вещь с id %d не существует у пользователя с id %d";
+        Optional<Item> byId = itemRepository.findById(itemId);
+
+        if (userId != null) {
+            if (byId.isEmpty()) {
+                throw new ItemServiceException(String.format(s, itemId, userId));
+            }
+            if (byId.get().getOwner().getId().equals(userId)) {
+                return byId.get();
+            }
+            throw new ItemServiceException(String.format(s, itemId, userId));
+        } else {
+            if (byId.isEmpty()) {
+                throw new ItemServiceException(String.format(s.substring(0, 26), itemId));
+            }
+            return byId.get();
+        }
     }
 }
