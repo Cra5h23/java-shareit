1207a460abce34fc9a9b60fc745f5685af5129d6
Nikolay <Novizdarialokin@yandex.ru>
2024-05-02 10:54:12 +0600

    feat: Добавил аннотации jpa

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,31 +1,50 @@
 package ru.practicum.shareit.user.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
+import javax.persistence.*;
 
 /**
  * Модель пользователя.
  *
  * @author Nikolay Radzivon
  */
-@Data
+@Getter
+@Setter
+@ToString
 @Builder(toBuilder = true)
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
     /**
      * Идентификационный номер пользователя.
      */
     @Builder.Default
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id = null;
     /**
      * Имя пользователя.
      */
+    @Column(name = "name")
     private String name;
     /**
      * Электронная почта пользователя.
      */
+    @Column(name = "email", unique = true, length = 512)
     private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        return id != null && id.equals(((User) o).getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
