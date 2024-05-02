cf4d9adef3a14ead8d3d186a027708c0ce77f9db
Nikolay <Novizdarialokin@yandex.ru>
2024-05-02 17:26:29 +0600

    feat: Добавил enum BookingStatus для статусов Бронирования

---
+++ src/main/java/ru/practicum/shareit/booking/model/BookingStatus.java
@@ -0,0 +1,12 @@
+package ru.practicum.shareit.booking.model;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 02.05.2024
+ */
+public enum BookingStatus {
+    WAITING,
+    APPROVED,
+    REJECTED,
+    CANCELED
+}
