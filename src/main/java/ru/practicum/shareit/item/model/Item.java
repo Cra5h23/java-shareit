fa5ad7ecfa25863daa516f99c63f7ac94343c176
Nikolay <Novizdarialokin@yandex.ru>
2024-05-02 11:06:23 +0600

    feat: Добавил аннотации jpa

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -4,38 +4,61 @@ import lombok.*;
 
 import ru.practicum.shareit.user.model.User;
 
+import javax.persistence.*;
+
 /**
  * Модель вещь.
  *
  * @author Nikolay Radzivon
  */
-
-@Data
+@Getter
+@Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
+@Entity
+@Table(name = "items")
 public class Item {
     /**
      * Уникальный идентификационный номер вещи
      */
     @Builder.Default
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id = null;
     /**
      * Имя вещи.
      */
-
+    @Column(name = "name", nullable = false)
     private String name;
     /**
      * Описание вещи.
      */
+    @Column(name = "description", length = 512, nullable = false)
     private String description;
     /**
      * Доступность вещи для аренды.
      */
+    @Column(name = "available", nullable = false)
     private Boolean available;
     /**
      * Пользователь владелец вещи.
      */
     @Builder.Default
+    @ManyToOne(fetch = FetchType.LAZY)
+    @ToString.Exclude
     private User owner = null;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Item)) return false;
+        return id != null && id.equals(((Item) o).getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
