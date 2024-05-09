bb6fdc06d150181f36c8b1813b7a563698e6c21c
Nikolay <Novizdarialokin@yandex.ru>
2024-05-09 14:24:11 +0600

    feat: Добавил параметр timezone

--- src/main/java/ru/practicum/shareit/booking/service/BookingService.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingService.java
@@ -31,34 +31,38 @@ public interface BookingService {
      * @param bookingId объект класса {@link Long} идентификационный номер бронирования.
      * @param userId    объект класса {@link Long} идентификационный номер пользователя.
      * @param approved  объект класса {@link Boolean} подтверждение бронирования.
+     * @param timeZone  объект класса {@link TimeZone} часовой пояс пользователя владельца вещи.
      * @return объект класса {@link BookingResponseDto}.
      */
-    BookingResponseDto bookingConfirmation(Long bookingId, Long userId, Boolean approved);
+    BookingResponseDto bookingConfirmation(Long bookingId, Long userId, Boolean approved, TimeZone timeZone);
 
     /**
      * Метод получения бронирования для указанного пользователя.
      *
      * @param bookingId объект класса {@link Long} идентификационный номер бронирования.
      * @param userId    {@link Long} идентификационный номер пользователя.
+     * @param timeZone  объект класса {@link TimeZone} часовой пояс бронирующего пользователя.
      * @return объект класса {@link BookingResponseDto}.
      */
-    BookingResponseDto getBooking(Long bookingId, Long userId);
+    BookingResponseDto getBooking(Long bookingId, Long userId, TimeZone timeZone);
 
     /**
      * Метод получения списка бронирований для бронирующего.
      *
-     * @param userId объект класса  {@link Long} идентификационный номер пользователя.
-     * @param state  объект класса {@link BookingState} состояние бронирования по которому запросить список.
+     * @param userId   объект класса  {@link Long} идентификационный номер пользователя.
+     * @param state    объект класса {@link BookingState} состояние бронирования по которому запросить список.
+     * @param timeZone объект класса {@link TimeZone} часовой пояс пользователя бронирующего вещь.
      * @return {@link List} объектов класса {@link BookingResponseDto}.
      */
-    List<BookingResponseDto> getBookingsByBooker(Long userId, BookingState state);
+    List<BookingResponseDto> getBookingsByBooker(Long userId, BookingState state, TimeZone timeZone);
 
     /**
      * Метод получения списка бронирований для владельца вещей.
      *
-     * @param userId объект класса  {@link Long} идентификационный номер пользователя владельца вещей.
-     * @param state  объект класса {@link BookingState} состояние бронирования по которому запросить список.
+     * @param userId   объект класса  {@link Long} идентификационный номер пользователя владельца вещей.
+     * @param state    объект класса {@link BookingState} состояние бронирования по которому запросить список.
+     * @param timeZone объект класса {@link TimeZone} часовой пояс пользователя владельца вещей.
      * @return {@link List} объектов класса {@link BookingResponseDto}.
      */
-    List<BookingResponseDto> getBookingByOwner(Long userId, BookingState state);
+    List<BookingResponseDto> getBookingByOwner(Long userId, BookingState state, TimeZone timeZone);
 }
