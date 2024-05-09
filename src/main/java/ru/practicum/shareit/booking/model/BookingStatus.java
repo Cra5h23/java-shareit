82a9cfff85318089b1d72031e6ca7e03b9ff066d
Nikolay <Novizdarialokin@yandex.ru>
2024-05-09 09:56:43 +0600

    feat: Добавил комментарии.

--- src/main/java/ru/practicum/shareit/booking/model/BookingStatus.java
+++ src/main/java/ru/practicum/shareit/booking/model/BookingStatus.java
@@ -1,12 +1,26 @@
 package ru.practicum.shareit.booking.model;
 
 /**
+ * Статус бронирования.
+ *
  * @author Nikolay Radzivon
  * @Date 02.05.2024
  */
 public enum BookingStatus {
+    /**
+     * Бронирование ожидает одобрение
+     */
     WAITING,
+    /**
+     * Бронирование подтверждено владельцем
+     */
     APPROVED,
+    /**
+     * Бронирование отклонено владельцем
+     */
     REJECTED,
+    /**
+     * Бронирование отменено создателем
+     */
     CANCELED
 }
