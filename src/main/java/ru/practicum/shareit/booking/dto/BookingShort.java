9858b296f5634bde4ad7ab299ebe2074ca9ed5b7
Nikolay <Novizdarialokin@yandex.ru>
2024-05-09 12:27:26 +0600

    feat: Добавил класс BookingShort

---
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingShort.java
@@ -0,0 +1,28 @@
+package ru.practicum.shareit.booking.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Класс {@link BookingShort} для полей lastBooking и nextBooking класса {@link ru.practicum.shareit.item.dto.OwnerItemResponseDto}
+ *
+ * @author Nikolay Radzivon
+ * @Date 06.05.2024
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class BookingShort {
+    /**
+     * Идентификационный номер бронирования.
+     */
+    private Long id;
+
+    /**
+     * Идентификационный номер пользователя букера.
+     */
+    private Long bookerId;
+}
