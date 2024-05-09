2145049497c2646c9a0aba9b7940b042d6330388
Nikolay <Novizdarialokin@yandex.ru>
2024-05-09 11:34:15 +0600

    feat: Реализовал метод addNewBooking

--- src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit.booking.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.Marker;
@@ -11,9 +12,10 @@ import ru.practicum.shareit.booking.dto.BookingRequestDto;
 import ru.practicum.shareit.booking.service.BookingService;
 
 import javax.validation.Valid;
+import java.util.TimeZone;
 
 /**
- * TODO Sprint add-bookings.
+ * Контроллер для работы с эндпоинтами /bookings.
  */
 @RestController
 @RequestMapping(path = "/bookings")
@@ -21,16 +23,29 @@ import javax.validation.Valid;
 @Slf4j
 @Validated
 public class BookingController {
-    private BookingService bookingService;
+    private final BookingService bookingService;
     private final String xSharerUserId = "X-Sharer-User-Id";
 
+    /**
+     * Метод для эндпоинта POST /bookings создание нового бронирования.
+     *
+     * @param booking  {@link BookingRequestDto} данные бронирования.
+     * @param userId   {@link Long} идентификационный номер пользователя букера.
+     * @param timeZone {@link TimeZone} часовой пояс пользователя.
+     * @return {@link ResponseEntity}
+     */
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
     @Validated(Marker.OnCreate.class)
-    public BookingDto addNewBooking(
+    public ResponseEntity<?> addNewBooking(
             @Valid @RequestBody BookingRequestDto booking,
-            @RequestHeader(value = xSharerUserId) Long userId) {
+            @RequestHeader(value = xSharerUserId) Long userId,
+            TimeZone timeZone) {
         log.info("POST /bookings, body = {}, header \"{}\" = {}", booking, xSharerUserId, userId);
         return bookingService.addNewBooKing(booking, userId);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(bookingService.addNewBooking(booking, userId, timeZone));
+    }
     }
 }
