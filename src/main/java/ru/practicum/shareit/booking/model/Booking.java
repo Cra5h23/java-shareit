9254930de85fd667b3a8320eb68639784a7950e2
Nikolay <Novizdarialokin@yandex.ru>
2024-05-09 09:52:53 +0600

    feat: Реализовал модель бронирование.

--- src/main/java/ru/practicum/shareit/booking/model/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -5,10 +5,10 @@ import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
 import javax.persistence.*;
-import java.time.Instant;
+import java.time.ZonedDateTime;
 
 /**
- * TODO Sprint add-bookings.
+ * Модель бронирование.
  */
 @Getter
 @Setter
@@ -19,28 +19,60 @@ import java.time.Instant;
 @Entity
 @Table(name = "bookings")
 public class Booking {
+    /**
+     * Уникальный идентификационный номер бронирования.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Builder.Default
+    private Long id = null;
 
-    @Column(name = " ")
-    private Instant start;
+    /**
+     * Дата и время начала бронирования.
+     */
+    @Column(name = "start_date")
+    private ZonedDateTime start;
 
-    @Column(name = "")
-    private Instant end;
+    /**
+     * Дата и время окончания бронирования.
+     */
+    @Column(name = "end_date")
+    private ZonedDateTime end;
 
-    @OneToOne(fetch = FetchType.LAZY, targetEntity = Item.class)
+    /**
+     * Вещь котору бронируют.
+     */
+    @ManyToOne(fetch = FetchType.LAZY, targetEntity = Item.class)
     @JoinColumn(name = "item_id")
     @Builder.Default
     @ToString.Exclude
     private Item item = null;
 
+    /**
+     * Пользователь, который бронирует.
+     */
     @ManyToOne(fetch = FetchType.LAZY, targetEntity = User.class)
     @JoinColumn(name = "user_id")
     @Builder.Default
     @ToString.Exclude
     private User booker = null;
 
+    /**
+     * Статус бронирования.
+     */
     @Enumerated(EnumType.STRING)
-    private BookingStatus status;
+    @Builder.Default
+    private BookingStatus status = BookingStatus.WAITING;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Booking)) return false;
+        return id != null && id.equals(((Booking) o).getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
