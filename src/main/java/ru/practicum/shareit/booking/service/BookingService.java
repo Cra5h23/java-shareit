d692552cbbbf5bf882499a8d1d1e73d412fe59b4
Nikolay <Novizdarialokin@yandex.ru>
2024-05-09 10:46:05 +0600

    feat: Добавил метод addNewBooking

--- src/main/java/ru/practicum/shareit/booking/service/BookingService.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingService.java
@@ -1,8 +1,27 @@
 package ru.practicum.shareit.booking.service;
 
+import ru.practicum.shareit.booking.dto.BookingResponseDto;
+import ru.practicum.shareit.booking.dto.BookingRequestDto;
+import ru.practicum.shareit.booking.model.BookingState;
+
+import java.util.List;
+import java.util.TimeZone;
+
 /**
+ * Интерфейс {@link BookingService} логики работы с бронированиями.
+ *
  * @author Nikolay Radzivon
  * @Date 02.05.2024
  */
 public interface BookingService {
+
+    /**
+     * Метод добавления нового бронирования.
+     *
+     * @param booking  объект класса {@link BookingRequestDto} данные для бронирования.
+     * @param userId   объект класса {@link Long} идентификационный номер бронирующего пользователя.
+     * @param timeZone объект класса {@link TimeZone} часовой пояс бронирующего пользователя.
+     * @return объект класса {@link BookingResponseDto}.
+     */
+    BookingResponseDto addNewBooking(BookingRequestDto booking, Long userId, TimeZone timeZone);
 }
