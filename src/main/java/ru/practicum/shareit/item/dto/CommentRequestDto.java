6f542d347a4c4aed6bd9a01344c879e53e61fd7e
Nikolay <Novizdarialokin@yandex.ru>
2024-05-10 16:08:04 +0600

    feat: Добавил класс CommentRequestDto

---
+++ src/main/java/ru/practicum/shareit/item/dto/CommentRequestDto.java
@@ -0,0 +1,29 @@
+package ru.practicum.shareit.item.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.Marker;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+/**
+ * Dto для класса {@link ru.practicum.shareit.item.model.Comment} для данных комментариев получаемых из запроса.
+ *
+ * @author Nikolay Radzivon
+ * @Date 07.05.2024
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class CommentRequestDto {
+    /**
+     * Текст комментария.
+     */
+    @NotBlank(groups = Marker.OnCreate.class)
+    @Size(min = 1, max = 512, groups = Marker.OnCreate.class)
+    private String text;
+}
