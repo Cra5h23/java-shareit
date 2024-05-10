3913115949f7afe56e97c1dfa1e66503f7886e4b
Nikolay <Novizdarialokin@yandex.ru>
2024-05-10 15:45:30 +0600

    feat: Добавил файл конфигурации OpenApiConfig

--- src/main/java/ru/practicum/shareit/configuration/OpenApiConfig.java
+++ src/main/java/ru/practicum/shareit/configuration/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package ru.practicum.shareit;
+package ru.practicum.shareit.configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
