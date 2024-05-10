5c7f1edbdd1777a946df1f0062165aae0d2ce591
Nikolay <Novizdarialokin@yandex.ru>
2024-05-10 22:13:49 +0600

    feat: Добавил конвертер StringToUserSortConverter

---
+++ src/main/java/ru/practicum/shareit/user/converter/StringToUserSortConverter.java
@@ -0,0 +1,24 @@
+package ru.practicum.shareit.user.converter;
+
+import org.springframework.core.convert.converter.Converter;
+import ru.practicum.shareit.booking.model.BookingState;
+import ru.practicum.shareit.exception.BookingStateException;
+import ru.practicum.shareit.user.exeption.UserSortException;
+import ru.practicum.shareit.user.model.UserSort;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 10.05.2024
+ */
+public class StringToUserSortConverter implements Converter<String, UserSort> {
+    @Override
+    public UserSort convert(String source) {
+        try {
+            return UserSort.valueOf(source.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            String message = e.getMessage();
+            String ex = message.substring(message.lastIndexOf(".") + 1);
+            throw new UserSortException(ex);
+        }
+    }
+}
