9a1dd85c1a05ed9d21b7a8d22c2cc9fe5942d338
Nikolay <Novizdarialokin@yandex.ru>
2024-05-10 15:50:13 +0600

    feat: Добавил исключение для отсутствующего бронирования.

---
+++ src/main/java/ru/practicum/shareit/exception/NotFoundBookingException.java
@@ -0,0 +1,11 @@
+package ru.practicum.shareit.exception;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 06.05.2024
+ */
+public class NotFoundBookingException extends RuntimeException{
+    public NotFoundBookingException(String message) {
+        super(message);
+    }
+}
