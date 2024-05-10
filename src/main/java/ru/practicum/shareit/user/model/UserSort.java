cc5e5769bd2c6acb53e931a9adbbae31ebd5da0a
Nikolay <Novizdarialokin@yandex.ru>
2024-05-10 22:18:22 +0600

    feat: Список сортировок для пользователей.

---
+++ src/main/java/ru/practicum/shareit/user/model/UserSort.java
@@ -0,0 +1,15 @@
+package ru.practicum.shareit.user.model;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 10.05.2024
+ */
+public enum UserSort {
+    NONE,
+    ID_DESC,
+    ID_ASC,
+    NAME_DESC,
+    NAME_ASC,
+    EMAIL_DESC,
+    EMAIL_ASC
+}
