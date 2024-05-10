6a5f363b75d51537c9a0b5206a1db45d347a9d89
Nikolay <Novizdarialokin@yandex.ru>
2024-05-10 22:18:42 +0600

    feat: Исключение для UserSor

---
+++ src/main/java/ru/practicum/shareit/user/exeption/UserSortException.java
@@ -0,0 +1,11 @@
+package ru.practicum.shareit.user.exeption;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 10.05.2024
+ */
+public class UserSortException extends RuntimeException {
+    public UserSortException(String message) {
+        super(message);
+    }
+}
