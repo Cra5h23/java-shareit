bf7c31667c9ba574e20ca5675fd4ca84176f234e
Nikolay <Novizdarialokin@yandex.ru>
2024-05-10 18:50:49 +0600

    refactor: Теперь наследует JpaRepository

--- src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
@@ -1,45 +1,12 @@
 package ru.practicum.shareit.user.repository;
 
-import ru.practicum.shareit.user.dto.UserResponseDto;
+import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.user.model.User;
-
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
 
 /**
  * Интерфейс {@link UserRepository}.
  *
  * @author Nikolay Radzivon.
  */
-public interface UserRepository {
-    /**
-     * Метод добавления нового пользователя в репозиторий или обновления существующего.
-     *
-     * @param user объект класса {@link User} для добавления.
-     * @return объект класса {@link UserResponseDto}.
-     */
-    User save(User user);
-
-    /**
-     * Метод получения пользователя по id из репозитория.
-     *
-     * @param userId идентификационный номер пользователя.
-     * @return объект класса {@link UserResponseDto}.
-     */
-    Optional<User> findById(long userId);
-
-    /**
-     * Метод удаления пользователя по id из репозитория.
-     *
-     * @param userId идентификационный номер пользователя.
-     */
-    void deleteById(long userId);
-
-    /**
-     * Метод получения списка всех пользователей из репозитория.
-     *
-     * @return {@link List} объектов {@link UserResponseDto}.
-     */
-    Collection<User> findAll();
+public interface UserRepository extends JpaRepository<User, Long> {
 }
