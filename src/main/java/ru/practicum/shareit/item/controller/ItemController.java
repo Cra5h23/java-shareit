809f60a892cb6936ea528b85c84a78e59fc4d1d1
Nikolay <Novizdarialokin@yandex.ru>
2024-05-10 16:00:25 +0600

    refactor: Теперь используется ResponseEntity в качестве ответа

--- src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -6,15 +6,19 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.Marker;
+import ru.practicum.shareit.item.dto.CommentRequestDto;
 import ru.practicum.shareit.item.dto.ItemRequestDto;
 import ru.practicum.shareit.item.dto.ItemResponseDto;
+import ru.practicum.shareit.item.dto.OwnerItemResponseDto;
 import ru.practicum.shareit.item.service.ItemService;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.TimeZone;
 
 /**
  * Контроллер для работы с эндпоинтами /items
@@ -33,48 +37,55 @@ public class ItemController {
     private final String xSharerUserId = "X-Sharer-User-Id";
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
     @Validated(Marker.OnCreate.class)
     @Operation(summary = "Добавление новой вещи", description = "Позволяет добавить новую вещь")
-    public ItemResponseDto addNewItem(@Valid @RequestBody @Parameter(description = "Данные вещи") ItemRequestDto item,
-                                      @RequestHeader(value = xSharerUserId)
-                                      @Parameter(description = "Идентификационный номер пользователя владельца вещи")
-                                      Long userId) {
+    public ResponseEntity<?> addNewItem(
+            @Valid @RequestBody @Parameter(description = "Данные вещи") ItemRequestDto item,
+            @RequestHeader(value = xSharerUserId) @Parameter(
+                    description = "Идентификационный номер пользователя владельца вещи") Long userId) {
         log.info("POST /items , body = {}, header \"{}\" = {}", item, xSharerUserId, userId);
-        return itemService.addNewItem(item, userId);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(itemService.addNewItem(item, userId));
     }
 
     @PatchMapping("/{itemId}")
-    @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Обновление вещи", description = "Позволяет обновить вещь (может обновить только владелец вещи)")
-    public ItemResponseDto updateItem(@RequestBody @Parameter(description = "Данные вещи") ItemRequestDto item,
-                                      @PathVariable @Parameter(description = "Идентификационный номер вещи") Long itemId,
-                                      @RequestHeader(value = xSharerUserId)
-                                      @Parameter(description = "Идентификационный номер пользователя владельца вещи")
-                                      Long userId) {
+    public ResponseEntity<?> updateItem(
+            @RequestBody @Parameter(description = "Данные вещи") ItemRequestDto item,
+            @PathVariable @Parameter(description = "Идентификационный номер вещи") Long itemId,
+            @RequestHeader(value = xSharerUserId) @Parameter(
+                    description = "Идентификационный номер пользователя владельца вещи") Long userId) {
         log.info("PATCH /items/{} , body = {}, header \"{}\" = {}", itemId, item, xSharerUserId, userId);
-        return itemService.updateItem(item, userId, itemId);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(itemService.updateItem(item, userId, itemId));
     }
 
     @GetMapping("/{itemId}")
-    @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Получение вещи по id", description = "Просмотр информации о вещи может запросить любой пользователь")
-    public ItemResponseDto getItemById(@PathVariable @Parameter(description = "Идентификационный номер вещи") Long itemId,
-                                       @RequestHeader(value = xSharerUserId, required = false)
-                                       @Parameter(description = "Идентификационный номер пользователя")
-                                       Long userId) {
+    public ResponseEntity<?> getItemById(
+            @PathVariable @Parameter(description = "Идентификационный номер вещи") Long itemId,
+            @RequestHeader(value = xSharerUserId, required = false) @Parameter(
+                    description = "Идентификационный номер пользователя") Long userId) {
         log.info("GET /items/{} , header \"{}\" = {}", itemId, xSharerUserId, userId);
-        return itemService.getItemByItemId(itemId, userId);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(itemService.getItemByItemId(itemId, userId));
     }
 
     @DeleteMapping("/{itemId}")
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Удаление вещи по id", description = "Удаление вещи (может удалить только пользователь владелец вещи")
-    public void deleteItemByItemId(@PathVariable @Parameter(description = "Идентификационный номер вещи") Long itemId,
-                                   @RequestHeader(value = xSharerUserId)
-                                   @Parameter(description = "Идентификационный номер пользователя владельца вещи")
-                                   Long userId) {
+    public void deleteItemByItemId(
+            @PathVariable @Parameter(description = "Идентификационный номер вещи") Long itemId,
+            @RequestHeader(value = xSharerUserId) @Parameter(
+                    description = "Идентификационный номер пользователя владельца вещи") Long userId) {
         log.info("DELETE /items/{} , header \"{}\" = {}", itemId, xSharerUserId, userId);
+
         itemService.deleteItemByItemId(itemId, userId);
     }
 
@@ -82,32 +93,38 @@ public class ItemController {
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Удаление всех вещей пользователя",
             description = "Удаление всех вещей (Может удалить только пользователь владелец вещей)")
-    public void deleteAllItemByUser(@RequestHeader(value = xSharerUserId)
-                                    @Parameter(description = "Идентификационный номер пользователя владельца вещи")
-                                    Long userId) {
+    public void deleteAllItemByUser(
+            @RequestHeader(value = xSharerUserId) @Parameter(
+                    description = "Идентификационный номер пользователя владельца вещи") Long userId) {
         log.info("DELETE /items , header \"{}\" = {}", xSharerUserId, userId);
+
         itemService.deleteAllItemByUser(userId);
     }
 
     @GetMapping
-    @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Получение всех вещей пользователя",
             description = "Получение всех вещей пользователя (может запросить только пользователь владелец вещей)")
-    public List<ItemResponseDto> getAllItemByUser(@RequestHeader(value = xSharerUserId) @Parameter(
-            description = "Идентификационный номер пользователя владельца вещей") Long userId) {
+    public ResponseEntity<?> getAllItemByUser(
+            @RequestHeader(value = xSharerUserId) @Parameter(
+                    description = "Идентификационный номер пользователя владельца вещей") Long userId) {
         log.info("GET /items , header \"{}\" = {}", xSharerUserId, userId);
-        return itemService.getAllItemByUser(userId);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(itemService.getAllItemByUser(userId));
     }
 
     @GetMapping("/search")
-    @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Поиск вещей", description = ("Поиск вещей по указанному тексту. " +
             "Поиск происходит по названию или описанию вещи. Выводятся только вещи доступные для аренды"))
-    public List<ItemResponseDto> searchItemByText(@RequestParam @Parameter(description = "Текст для поиска") String text,
-                                                  @RequestHeader(value = xSharerUserId)
-                                                  @Parameter(description = "Идентификационный номер пользователя")
-                                                  Long userId) {
+    public ResponseEntity<?> searchItemByText(
+            @RequestParam @Parameter(description = "Текст для поиска") String text,
+            @RequestHeader(value = xSharerUserId) @Parameter(
+                    description = "Идентификационный номер пользователя") Long userId) {
         log.info("GET /items/search?text={} , header \"{}\" = {}", text, xSharerUserId, userId);
-        return itemService.searchItemByText(text, userId);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(itemService.searchItemByText(text, userId));
     }
 }
