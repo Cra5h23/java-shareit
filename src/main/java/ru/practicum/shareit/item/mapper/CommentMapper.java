2a595d4ad8de49274f5c5a84621003bf9c6db769
Nikolay <Novizdarialokin@yandex.ru>
2024-05-10 16:16:51 +0600

    feat: Добавил класс CommentMapper.

---
+++ src/main/java/ru/practicum/shareit/item/mapper/CommentMapper.java
@@ -0,0 +1,41 @@
+package ru.practicum.shareit.item.mapper;
+
+import ru.practicum.shareit.item.dto.CommentRequestDto;
+import ru.practicum.shareit.item.dto.CommentResponseDto;
+import ru.practicum.shareit.item.dto.ItemRequestDto;
+import ru.practicum.shareit.item.dto.ItemResponseDto;
+import ru.practicum.shareit.item.model.Comment;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDateTime;
+import java.util.TimeZone;
+
+/**
+ * Маппер для классов {@link Comment}, {@link CommentResponseDto} и {@link CommentRequestDto}.
+ * Предназначен для преобразования объекта класса {@link CommentRequestDto} в объект класса {@link Comment}
+ * и объект класса {@link Comment} в объект класса {@link CommentResponseDto}.
+ *
+ * @author Nikolay Radzivon
+ * @Date 07.05.2024
+ */
+public class CommentMapper {
+
+    public static Comment toComment(CommentRequestDto text, Item item, User user, TimeZone timeZone) {
+        return Comment.builder()
+                .author(user)
+                .item(item)
+                .created(LocalDateTime.now().atZone(timeZone.toZoneId()))
+                .text(text.getText())
+                .build();
+    }
+
+    public static CommentResponseDto toCommentResponseDto(Comment comment) {
+        return CommentResponseDto.builder()
+                .id(comment.getId())
+                .text(comment.getText())
+                .authorName(comment.getAuthor().getName())
+                .created(comment.getCreated().toLocalDateTime().toString())
+                .build();
+    }
+}
