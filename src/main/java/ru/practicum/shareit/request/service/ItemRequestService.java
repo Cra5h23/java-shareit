d8a0ecaa0cda4d63d8090cab465e1eaefc919e0b
Nikolay <Novizdarialokin@yandex.ru>
2024-05-18 12:50:45 +0600

    feat: добавил метод addNewRequest

--- src/main/java/ru/practicum/shareit/request/service/ItemRequestService.java
+++ src/main/java/ru/practicum/shareit/request/service/ItemRequestService.java
@@ -1,8 +1,24 @@
 package ru.practicum.shareit.request.service;
 
+import ru.practicum.shareit.request.dto.ItemRequestDtoRequest;
+import ru.practicum.shareit.request.dto.ItemRequestDtoResponse;
+
+import java.util.TimeZone;
+
 /**
+ * Интерфейс {@link ItemRequestService} для логики работы с запросами вещей.
+ *
  * @author Nikolay Radzivon
  * @Date 18.05.2024
  */
 public interface ItemRequestService {
+    /**
+     * Метод создания нового запроса вещи.
+     *
+     * @param request  {@link ItemRequestDtoRequest} описание вещи которая нужна.
+     * @param userId   {@link Long} идентификационный номер пользователя создающего запрос.
+     * @param timeZone {@link TimeZone} часовой пояс пользователя создающего запрос.
+     * @return {@link ItemRequestDtoResponse} данные запроса.
+     */
+    ItemRequestDtoResponse addNewRequest(ItemRequestDtoRequest request, Long userId, TimeZone timeZone);
 }
