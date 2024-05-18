f97ed8011d8eaec313d8a358a52dd4c98a13efbe
Nikolay <Novizdarialokin@yandex.ru>
2024-05-18 12:47:22 +0600

    feat: Добавил заготовку Dto класса ItemRequest использующуюся в качестве ответа.

---
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDtoResponse.java
@@ -0,0 +1,10 @@
+package ru.practicum.shareit.request.dto;
+
+/**
+ * Dto класса {@link ru.practicum.shareit.request.model.ItemRequest} для передачи данных в качестве ответа.
+ *
+ * @author Nikolay Radzivon
+ * @Date 18.05.2024
+ */
+public class ItemRequestDtoResponse {
+}
