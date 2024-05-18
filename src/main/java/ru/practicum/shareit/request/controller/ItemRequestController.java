4881f3003ab54cf1f1b216273733d02bbd196278
Nikolay <Novizdarialokin@yandex.ru>
2024-05-18 12:50:31 +0600

    feat: Реализовал метод addNewRequest

--- src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
@@ -1,13 +1,44 @@
 package ru.practicum.shareit.request.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.request.dto.ItemRequestDtoRequest;
+import ru.practicum.shareit.request.service.ItemRequestService;
+
+import java.util.TimeZone;
 
 /**
- * TODO Sprint add-item-requests.
+ * Контроллер для эндпоинтов /requests
+ *
+ * @author Nikolay Radzivon
  */
-
+@RequiredArgsConstructor
+@Slf4j
 @RestController
 @RequestMapping(path = "/requests")
 public class ItemRequestController {
+    private final ItemRequestService itemRequestService;
+    private final String xSharerUserId = "X-Sharer-User-Id";
+
+    /**
+     * Метод создания нового запроса вещи.
+     * @param request {@link ItemRequestDtoRequest} описание вещи которая требуется.
+     * @param userId {@link Long} идентификационный номер пользователя создающего запрос.
+     * @param timeZone {@link TimeZone} часовой пояс пользователя.
+     * @return {@link ResponseEntity}
+     */
+    @PostMapping
+    public ResponseEntity<?> addNewRequest(
+            @RequestBody ItemRequestDtoRequest request,
+            @RequestHeader(value = xSharerUserId) Long userId,
+            TimeZone timeZone) {
+        log.info("POST /requests , body = {} , {} = {}", request, xSharerUserId, userId);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(itemRequestService.addNewRequest(request, userId, timeZone));
+    }
 }
