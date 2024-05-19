31317affae499dabc1fdf984602f6495c61a1b24
Nikolay <Novizdarialokin@yandex.ru>
2024-05-19 16:35:25 +0600

    feat: добавил метод findAllByRequestorId

--- src/main/java/ru/practicum/shareit/request/repository/ItemRequestRepository.java
+++ src/main/java/ru/practicum/shareit/request/repository/ItemRequestRepository.java
@@ -1,11 +1,17 @@
 package ru.practicum.shareit.request.repository;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.practicum.shareit.request.model.ItemRequest;
+
+import java.util.List;
 
 /**
  * @author Nikolay Radzivon
  * @Date 19.05.2024
  */
 public interface ItemRequestRepository extends JpaRepository<ItemRequest, Long> {
+
+    List<ItemRequest> findAllByRequestorId(Long requestorId, Sort sort);
 }
