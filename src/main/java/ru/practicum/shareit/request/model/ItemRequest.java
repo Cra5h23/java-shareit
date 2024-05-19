31bc68d5e38d8c8012f744d0b146faf9cf743f4e
Nikolay <Novizdarialokin@yandex.ru>
2024-05-19 10:33:41 +0600

    feat: добавил модель запроса

--- src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
@@ -1,7 +1,61 @@
 package ru.practicum.shareit.request.model;
 
+import lombok.*;
+import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.user.model.User;
+
+import javax.persistence.*;
+import java.time.ZonedDateTime;
+
 /**
- * TODO Sprint add-item-requests.
+ * Модель запроса на вещь.
+ *
+ * @author Nikolay Radzivon
  */
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Entity
+@Table(name = "requests")
 public class ItemRequest {
+    /**
+     * Идентификационный номер запроса.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * Описание вещи которую хотят забронировать.
+     */
+    @Column(name = "description", nullable = false, length = 512)
+    private String description;
+
+    /**
+     * Пользователь создатель запроса.
+     */
+    @ManyToOne(targetEntity = User.class)
+    @JoinColumn(name = "requestor_id", nullable = false)
+    private User requestor;
+
+    /**
+     * Дата и время создания запроса.
+     */
+    @Column(name = "created_data", nullable = false)
+    private ZonedDateTime created;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Booking)) return false;
+        return id != null && id.equals(((Booking) o).getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
