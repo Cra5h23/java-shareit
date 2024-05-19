dc36f0675fb2ef658050f137fab8b06edc2d3ddb
Nikolay <Novizdarialokin@yandex.ru>
2024-05-19 16:32:59 +0600

    feat: Реализовал класс ItemRequestDtoResponse

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDtoResponse.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDtoResponse.java
@@ -1,10 +1,36 @@
 package ru.practicum.shareit.request.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 /**
  * Dto класса {@link ru.practicum.shareit.request.model.ItemRequest} для передачи данных в качестве ответа.
  *
  * @author Nikolay Radzivon
  * @Date 18.05.2024
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
 public class ItemRequestDtoResponse {
+    /**
+     * Описание запроса.
+     */
+    private String description;
+
+    /**
+     * Дата создания запроса.
+     */
+    private LocalDateTime created;
+
+    /**
+     *Список ответов на запрос.
+     */
+    private List<ItemFromItemRequest> response;
 }
