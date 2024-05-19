6033e26b99dff37a80d868c177912db67c716ec1
Nikolay <Novizdarialokin@yandex.ru>
2024-05-19 18:11:07 +0600

    feat: Добавил метод findAllByRequestorIdNot

--- src/main/java/ru/practicum/shareit/request/repository/ItemRequestRepository.java
+++ src/main/java/ru/practicum/shareit/request/repository/ItemRequestRepository.java
@@ -1,8 +1,9 @@
 package ru.practicum.shareit.request.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import ru.practicum.shareit.request.model.ItemRequest;
 
 import java.util.List;
@@ -14,4 +15,6 @@ import java.util.List;
 public interface ItemRequestRepository extends JpaRepository<ItemRequest, Long> {
 
     List<ItemRequest> findAllByRequestorId(Long requestorId, Sort sort);
+
+    Page<ItemRequest> findAllByRequestorIdNot(Long requestorId, Pageable pageable);
 }
