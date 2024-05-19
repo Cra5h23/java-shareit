0c3ef84af55a9a0accd847530514c2e71ceb4e3d
Nikolay <Novizdarialokin@yandex.ru>
2024-05-19 16:35:57 +0600

    feat: реализовал метод getUserRequests

--- src/main/java/ru/practicum/shareit/request/service/impl/ItemRequestServiceImpl.java
+++ src/main/java/ru/practicum/shareit/request/service/impl/ItemRequestServiceImpl.java
@@ -2,16 +2,20 @@ package ru.practicum.shareit.request.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import ru.practicum.shareit.UserChecker;
 import ru.practicum.shareit.request.dto.ItemRequestDtoCreated;
 import ru.practicum.shareit.request.dto.ItemRequestDtoRequest;
 import ru.practicum.shareit.request.dto.ItemRequestDtoResponse;
 import ru.practicum.shareit.request.mapper.ItemRequestMapper;
+import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.request.repository.ItemRequestRepository;
 import ru.practicum.shareit.request.service.ItemRequestService;
 
+import java.util.List;
 import java.util.TimeZone;
+import java.util.stream.Collectors;
 
 /**
  * @author Nikolay Radzivon
@@ -33,15 +37,38 @@ public class ItemRequestServiceImpl implements ItemRequestService {
      * @return {@link ItemRequestDtoResponse} данные запроса.
      */
     @Override
-    public ItemRequestDtoCreated addNewRequest(ItemRequestDtoRequest request, Long userId, TimeZone timeZone)  {
+    public ItemRequestDtoCreated addNewRequest(ItemRequestDtoRequest request, Long userId, TimeZone timeZone) {
         log.info("Пользователь с id {} создаёт запрос {}", userId, request);
 
-        var user = userChecker.checkUser(userId, String.format("Пользователь с id %d не существует", userId));
+        var user = userChecker.checkUser(userId, String.format(
+                "Нельзя создать новый запрос для не существующего пользователя с id %d.", userId));
         var itemRequest = ItemRequestMapper.toItemRequest(request, user, timeZone);
         var save = itemRequestRepository.save(itemRequest);
         var itemRequestDtoCreated = ItemRequestMapper.toItemRequestDtoCreated(save);
 
         log.info("Создан запрос {}", itemRequestDtoCreated);
         return itemRequestDtoCreated;
+    }
+
+    /**
+     * Метод получения всех запросов для указанного пользователя.
+     *
+     * @param userId {@link Long} идентификационный номер пользователя.
+     * @return список запросов.
+     */
+    @Override
+    public List<ItemRequestDtoResponse> getUserRequests(Long userId) {
+        userChecker.checkUser(userId, String.format(
+                "Нельзя запросить список запросов для не существующего пользователя с id %d.", userId));
+
+        var typedSort = Sort.sort(ItemRequest.class);
+        var sort = typedSort.by(ItemRequest::getCreated).descending();
+        var requests = itemRequestRepository.findAllByRequestorId(userId, sort);
+        var requestsDto = requests.stream()
+                .map(ItemRequestMapper::toItemRequestDtoResponse)
+                .collect(Collectors.toList());
+
+        log.info("Запрошен список всех запросов для пользователя с id {}", userId);
+        return requestsDto;
     }
 }
