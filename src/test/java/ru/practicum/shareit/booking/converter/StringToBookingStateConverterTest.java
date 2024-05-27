42f3d801bc6da6f42f91d1039c366102c535febf
Nikolay <Novizdarialokin@yandex.ru>
2024-05-27 18:19:02 +0600

    feat: Добавил тесты для StringToBookingStateConverter

--- src/test/java/ru/practicum/shareit/booking/converter/StringToBookingStateConverterTest.java
+++ src/test/java/ru/practicum/shareit/booking/converter/StringToBookingStateConverterTest.java
@@ -5,42 +5,40 @@ import org.junit.jupiter.api.Test;
 import ru.practicum.shareit.booking.model.BookingState;
 import ru.practicum.shareit.exception.BookingStateException;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 /**
  * @author Nikolay Radzivon
  * @Date 27.05.2024
  */
 class StringToBookingStateConverterTest {
 
-    private StringToBookingStateConverter converter = new StringToBookingStateConverter();
+    private final StringToBookingStateConverter converter = new StringToBookingStateConverter();
 
     @Test
     void convertTestValid() {
         var all = converter.convert("all");
-        Assertions.assertEquals(all, BookingState.ALL);
+        Assertions.assertEquals(BookingState.ALL, all);
 
         var current = converter.convert("current");
-        Assertions.assertEquals(current, BookingState.CURRENT);
+        Assertions.assertEquals(BookingState.CURRENT, current);
 
         var past = converter.convert("past");
-        Assertions.assertEquals(past, BookingState.PAST);
+        Assertions.assertEquals(BookingState.PAST, past);
 
         var future = converter.convert("future");
-        Assertions.assertEquals(future, BookingState.FUTURE);
+        Assertions.assertEquals(BookingState.FUTURE, future);
 
         var waiting = converter.convert("waiting");
-        Assertions.assertEquals(waiting, BookingState.WAITING);
+        Assertions.assertEquals(BookingState.WAITING, waiting);
 
         var rejected = converter.convert("rejected");
-        Assertions.assertEquals(rejected, BookingState.REJECTED);
+        Assertions.assertEquals(BookingState.REJECTED, rejected);
     }
 
     @Test
     void convertTestNotValid() {
         var source = "asdfg";
 
-        Throwable throwable = Assertions.assertThrows(BookingStateException.class,()-> converter.convert(source));
+        Throwable throwable = Assertions.assertThrows(BookingStateException.class, () -> converter.convert(source));
 
         Assertions.assertNotNull(throwable);
         Assertions.assertNotNull(throwable.getMessage());
