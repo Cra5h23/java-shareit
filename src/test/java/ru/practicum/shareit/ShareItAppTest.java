478d882013847122a9013fd7f954309121ac300a
Nikolay <Novizdarialokin@yandex.ru>
2024-05-27 10:15:03 +0600

    feat: Добавил тесты для ShareItApp

---
+++ src/test/java/ru/practicum/shareit/ShareItAppTest.java
@@ -0,0 +1,17 @@
+package ru.practicum.shareit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 26.05.2024
+ */
+public class ShareItAppTest {
+
+    @Test
+    void MainTest() {
+        Assertions.assertDoesNotThrow(ShareItApp::new);
+        Assertions.assertDoesNotThrow(()-> ShareItApp.main(new String[]{}));
+    }
+}
