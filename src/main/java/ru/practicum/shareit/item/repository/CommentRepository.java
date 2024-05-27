79d71920471d9d0276e4298498fb2d9f0f685e31
Nikolay <Novizdarialokin@yandex.ru>
2024-05-27 18:08:14 +0600

    refactor: Удалил findByItem_id так как больше не используется

--- src/main/java/ru/practicum/shareit/item/repository/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/CommentRepository.java
@@ -3,8 +3,6 @@ package ru.practicum.shareit.item.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.item.model.Comment;
 
-import java.util.List;
-
 /**
  * Интерфейс {@link CommentRepository}
  *
@@ -12,11 +10,4 @@ import java.util.List;
  * @Date 07.05.2024
  */
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    /**
-     * Метод получения списка комментариев по id вещи.
-     *
-     * @param itemId идентификационный номер вещи.
-     * @return {@link List} {@link Comment}
-     */
-    List<Comment> findByItem_id(Long itemId);
 }
