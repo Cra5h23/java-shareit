a50117756834813d5483c69ecfbaa5c037d6054a
Nikolay <Novizdarialokin@yandex.ru>
2024-05-27 13:53:56 +0600

    feat: Добавил тесты для BookingState

---
+++ src/test/java/ru/practicum/shareit/booking/model/BookingStateTest.java
@@ -0,0 +1,28 @@
+package ru.practicum.shareit.booking.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 27.05.2024
+ */
+class BookingStateTest {
+    @Test
+    void valueOfTest() {
+        Assertions.assertEquals(BookingState.ALL, BookingState.valueOf("ALL"));
+        Assertions.assertEquals(BookingState.WAITING, BookingState.valueOf("WAITING"));
+        Assertions.assertEquals(BookingState.CURRENT, BookingState.valueOf("CURRENT"));
+        Assertions.assertEquals(BookingState.REJECTED, BookingState.valueOf("REJECTED"));
+        Assertions.assertEquals(BookingState.FUTURE, BookingState.valueOf("FUTURE"));
+        Assertions.assertEquals(BookingState.PAST, BookingState.valueOf("PAST"));
+
+        Throwable throwable = Assertions.assertThrows(IllegalArgumentException.class, () -> BookingState.valueOf("sdad"));
+
+        Assertions.assertNotNull(throwable);
+        Assertions.assertNotNull(throwable.getMessage());
+        Assertions.assertEquals("No enum constant ru.practicum.shareit.booking.model.BookingState.sdad", throwable.getMessage());
+    }
+}
