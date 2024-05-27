e0f02dd1315c9175cc7716587959d58044bc234b
Nikolay <Novizdarialokin@yandex.ru>
2024-05-27 17:25:46 +0600

    feat: Добавил тесты для ItemRequestMapper

--- src/test/java/ru/practicum/shareit/request/mapper/ItemRequestMapperTest.java
+++ src/test/java/ru/practicum/shareit/request/mapper/ItemRequestMapperTest.java
@@ -2,13 +2,16 @@ package ru.practicum.shareit.request.mapper;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.request.dto.ItemRequestDtoRequest;
 import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.request.model.ItemResponse;
 import ru.practicum.shareit.user.model.User;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.List;
 import java.util.TimeZone;
 
 /**
@@ -69,6 +72,21 @@ class ItemRequestMapperTest {
                         .email("testUser@email.com")
                         .build())
                 .description("testDescription")
+                .responses(List.of(ItemResponse.builder()
+                        .id(23L)
+                        .item(Item.builder()
+                                .id(13L)
+                                .owner(User.builder()
+                                        .id(3L)
+                                        .name("testUser3")
+                                        .email("testUser3@email.com")
+                                        .build())
+                                .name("testItem")
+                                .description("testDescription")
+                                .available(true)
+                                .build())
+                        .request(ItemRequest.builder().id(1L).build())
+                        .build()))
                 .build();
 
         var test = ItemRequestMapper.toItemRequestDtoResponse(request);
@@ -77,6 +95,10 @@ class ItemRequestMapperTest {
         Assertions.assertEquals(LocalDateTime.of(2024, 3, 13, 13, 23, 0), test.getCreated());
         Assertions.assertEquals(1, test.getId());
         Assertions.assertEquals("testDescription", test.getDescription());
-        Assertions.assertNull(test.getItems());
+        Assertions.assertEquals(13,test.getItems().get(0).getId());
+        Assertions.assertEquals(1,test.getItems().get(0).getRequestId());
+        Assertions.assertEquals("testDescription",test.getItems().get(0).getDescription());
+        Assertions.assertEquals("testItem",test.getItems().get(0).getName());
+        Assertions.assertEquals(true,test.getItems().get(0).getAvailable());
     }
 }
