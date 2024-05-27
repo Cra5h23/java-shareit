5f668bfce60c360b3663ef7473f994f0dd5d4a0e
Nikolay <Novizdarialokin@yandex.ru>
2024-05-27 18:45:12 +0600

    fix: пробел

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.item.model;
 
 import lombok.*;
-
 import ru.practicum.shareit.user.model.User;
 
 import javax.persistence.*;
@@ -55,7 +54,7 @@ public class Item {
 
     @OneToMany(mappedBy = "item", fetch = FetchType.LAZY)
     @ToString.Exclude
-    private List<Comment> comments =new ArrayList<>();
+    private List<Comment> comments = new ArrayList<>();
 
     @Override
     public boolean equals(Object o) {
