eb9d3156c7162a2b678d31e4679f9d36511178e6
Nikolay <Novizdarialokin@yandex.ru>
2024-05-27 19:22:51 +0600

    fix: equals

--- src/main/java/ru/practicum/shareit/request/model/ItemResponse.java
+++ src/main/java/ru/practicum/shareit/request/model/ItemResponse.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.request.model;
 
 import lombok.*;
-import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.item.model.Item;
 
 import javax.persistence.*;
