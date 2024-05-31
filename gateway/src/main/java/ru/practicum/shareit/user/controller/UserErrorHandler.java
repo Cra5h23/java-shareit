71833786e818c53d0ee93408f05a3c3b4ac20bc6
Nikolay <Novizdarialokin@yandex.ru>
2024-05-31 17:31:59 +0600

    feat: добавил класс UserErrorHandler

---
+++ gateway/src/main/java/ru/practicum/shareit/user/controller/UserErrorHandler.java
@@ -0,0 +1,26 @@
+package ru.practicum.shareit.user.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import ru.practicum.shareit.user.converter.StringToUserSortConverter;
+
+import static ru.practicum.shareit.exception.ErrorResponse.makeErrorResponse;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 31.05.2024
+ */
+@Slf4j
+@ControllerAdvice("ru.practicum.shareit.user")
+public class UserErrorHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<?> handlerBookingStateException(final StringToUserSortConverter.UserSortException e, WebRequest webRequest) {
+        log.warn("Ошибка ввода типа сортировки", e);
+        return makeErrorResponse(webRequest, HttpStatus.BAD_REQUEST, "Unknown sort: ");
+    }
+}
