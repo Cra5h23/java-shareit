3465266cbce11a7770bc4b3e4f61e051926a3cbe
Nikolay <Novizdarialokin@yandex.ru>
2024-05-31 17:29:48 +0600

    feat: добавил класс interface Marker

---
+++ gateway/src/main/java/ru/practicum/shareit/Marker.java
@@ -0,0 +1,13 @@
+package ru.practicum.shareit;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 19.04.2024
+ */
+public interface Marker {
+    interface OnCreate {
+    }
+
+    interface OnUpdate {
+    }
+}
