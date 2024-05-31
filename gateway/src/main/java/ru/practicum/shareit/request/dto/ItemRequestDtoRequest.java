71b68ed12641b14ef3a27a499940275f1df9abea
Nikolay <Novizdarialokin@yandex.ru>
2024-05-31 17:29:28 +0600

    feat: добавил класс ItemRequestDtoRequest

---
+++ gateway/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDtoRequest.java
@@ -0,0 +1,28 @@
+package ru.practicum.shareit.request.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.Marker;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+/**
+ * Dto для модели {@link ru.practicum.shareit.request.model.ItemRequest} использующееся для получения данных из запроса.
+ *
+ * @author Nikolay Radzivon
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class ItemRequestDtoRequest {
+    /**
+     * Описание вещи.
+     */
+    @NotBlank(groups = Marker.OnCreate.class, message = "Описание требуемой вещи не может быть пустым")
+    @Size(min = 1, max = 512, message = "Описание требуемой вещи не может быть меньше 1 и больше 512 символов")
+    private String description;
+}
