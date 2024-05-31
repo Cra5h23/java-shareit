d0514e28029c38c06422b76ad7f2f647c06da797
Nikolay <Novizdarialokin@yandex.ru>
2024-05-31 18:04:13 +0600

    feat: добавил тесты

---
+++ gateway/src/test/java/ru/practicum/shareit/ShareItGatewayTest.java
@@ -0,0 +1,15 @@
+package ru.practicum.shareit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 31.05.2024
+ */
+@SpringBootTest
+class ShareItGatewayTest {
+    @Test
+    void contextLoads() {
+    }
+}
