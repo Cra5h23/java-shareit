5a8787971b073e477157f136a2ab6a6b59e692fe
Nikolay <Novizdarialokin@yandex.ru>
2024-05-31 17:23:17 +0600

    feat: добавил класс BookingErrorHandler

---
+++ gateway/src/main/java/ru/practicum/shareit/booking/controller/BookingErrorHandler.java
@@ -0,0 +1,25 @@
+package ru.practicum.shareit.booking.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import ru.practicum.shareit.booking.converter.StringToBookingStateConverter;
+
+import static ru.practicum.shareit.exception.ErrorResponse.makeErrorResponse;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 31.05.2024
+ */
+@ControllerAdvice("ru.practicum.shareit.booking")
+@Slf4j
+public class BookingErrorHandler {
+    @ExceptionHandler
+    public ResponseEntity<?> handlerBookingStateException(final StringToBookingStateConverter.BookingStateException e, WebRequest webRequest) {
+        log.warn("Ошибка ввода данных бронирования", e);
+        return makeErrorResponse(webRequest, HttpStatus.BAD_REQUEST, "Unknown state: ");
+    }
+}
