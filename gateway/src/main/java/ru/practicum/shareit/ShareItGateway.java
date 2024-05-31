8e2220368b973eff239994ee3fb1e601133ece06
Nikolay <Novizdarialokin@yandex.ru>
2024-05-31 16:53:25 +0600

    feat: Добавил мэйн класс для модуля gateway ShareItGateway

---
+++ gateway/src/main/java/ru/practicum/shareit/ShareItGateway.java
@@ -0,0 +1,15 @@
+package ru.practicum.shareit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 30.05.2024
+ */
+@SpringBootApplication
+public class ShareItGateway {
+    public static void main(String[] args) {
+        SpringApplication.run(ShareItGateway.class, args);
+    }
+}
