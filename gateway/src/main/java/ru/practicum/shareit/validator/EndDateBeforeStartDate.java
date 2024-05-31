6809d84e5d653c57944a52ae1d413afe17f92996
Nikolay <Novizdarialokin@yandex.ru>
2024-05-31 17:26:35 +0600

    feat: добавил EndDateBeforeStartDate

---
+++ gateway/src/main/java/ru/practicum/shareit/validator/EndDateBeforeStartDate.java
@@ -0,0 +1,24 @@
+package ru.practicum.shareit.validator;
+
+import javax.validation.Constraint;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Аннотация проверки для класса {@link ru.practicum.shareit.booking.dto.BookingRequestDto}, что дата окончания бронирования не должна быть раньше начала бронирования.
+ *
+ * @author Nikolay Radzivon
+ * @Date 03.05.2024
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = EndDateBeforeStartDateValidator.class)
+public @interface EndDateBeforeStartDate {
+    String message() default "Дата окончания бронирования не должна быть раньше даты начала бронирования";
+
+    Class<?>[] groups() default {};
+
+    Class<?>[] payload() default {};
+}
