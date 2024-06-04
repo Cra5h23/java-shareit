89bb44f6582a5e4c11ced18d6f2a5d40bb3d1459
Nikolay <Novizdarialokin@yandex.ru>
2024-06-04 12:50:23 +0600

    feat: добавил тесты для UserSort

---
+++ gateway/src/test/java/ru/practicum/shareit/user/dto/UserSortTest.java
@@ -0,0 +1,36 @@
+package ru.practicum.shareit.user.dto;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 03.06.2024
+ */
+class UserSortTest {
+
+    @Test
+    void valuesTest() {
+        UserSort[] values = UserSort.values();
+
+        Assertions.assertNotNull(values);
+        Assertions.assertEquals(7, values.length);
+    }
+
+    @Test
+    void valueOfTest() {
+        Assertions.assertEquals(UserSort.NONE, UserSort.valueOf("NONE"));
+        Assertions.assertEquals(UserSort.NAME_DESC, UserSort.valueOf("NAME_DESC"));
+        Assertions.assertEquals(UserSort.ID_ASC, UserSort.valueOf("ID_ASC"));
+        Assertions.assertEquals(UserSort.ID_DESC, UserSort.valueOf("ID_DESC"));
+        Assertions.assertEquals(UserSort.NAME_ASC, UserSort.valueOf("NAME_ASC"));
+        Assertions.assertEquals(UserSort.EMAIL_ASC, UserSort.valueOf("EMAIL_ASC"));
+        Assertions.assertEquals(UserSort.EMAIL_DESC, UserSort.valueOf("EMAIL_DESC"));
+
+        Throwable throwable = Assertions.assertThrows(IllegalArgumentException.class, () -> UserSort.valueOf("sdad"));
+
+        Assertions.assertNotNull(throwable);
+        Assertions.assertNotNull(throwable.getMessage());
+        Assertions.assertEquals("No enum constant ru.practicum.shareit.user.dto.UserSort.sdad", throwable.getMessage());
+    }
+}
