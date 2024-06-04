7bc986cf3896b13a7207b56e8d1bb55f518748b6
Nikolay <Novizdarialokin@yandex.ru>
2024-06-04 12:41:39 +0600

    feat: добавил ItemDtoRequest

--- gateway/src/main/java/ru/practicum/shareit/item/dto/ItemDtoRequest.java
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/ItemDtoRequest.java
@@ -1,16 +1,16 @@
 package ru.practicum.shareit.item.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ru.practicum.shareit.Marker;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 /**
- * Dto класса {@link ru.practicum.shareit.item.model.Item} для принятия данных вещи из запроса.
+ * Dto {@link ItemDtoRequest} для принятия данных вещи из запроса.
  *
  * @author Nikolay Radzivon
  * @Date 20.04.2024
@@ -19,31 +19,27 @@ import javax.validation.constraints.NotNull;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder(toBuilder = true)
-@Schema(name = "Вещь для запроса", description = "Сущность вещи для запроса")
 public class ItemDtoRequest {
     /**
      * Название вещи.
      */
-    @NotBlank(/*groups = Marker.OnCreate.class,*/ message = "Название предмета не может быть пустым")
-    @Schema(description = "Название вещи", example = "exampleName")
+    @NotBlank(groups = Marker.OnCreate.class, message = "Название предмета не может быть пустым")
     private String name;
 
     /**
      * Описание вещи.
      */
-    @NotBlank(/*groups = Marker.OnCreate.class,*/ message = "Описание предмета не может быть пустым")
-    @Schema(description = "Описание вещи", example = "exampleDescription")
+    @NotBlank(groups = Marker.OnCreate.class, message = "Описание предмета не может быть пустым")
     private String description;
 
     /**
      * Возможность аренды вещи.
      */
-    @NotNull(/*groups = Marker.OnCreate.class,*/ message = "Возможность аренды должна быть указана")
-    @Schema(description = "Доступность для аренды", example = "true")
+    @NotNull(groups = Marker.OnCreate.class, message = "Возможность аренды должна быть указана")
     private Boolean available;
 
     /**
-     * Идентификационный номер запроса до которого создана вещь.
+     * Идентификационный номер запроса для которого создана вещь.
      */
     private Long requestId;
 }
