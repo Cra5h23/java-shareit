2ad0af7c0d8143c1a61a901beb9398f620c009d7
Nikolay <Novizdarialokin@yandex.ru>
2024-06-04 12:47:58 +0600

    feat: Добавил тесты для StringToUserSortConverter

---
+++ gateway/src/test/java/ru/practicum/shareit/user/converter/StringToUserSortConverterTest.java
@@ -0,0 +1,46 @@
+package ru.practicum.shareit.user.converter;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.practicum.shareit.user.dto.UserSort;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 03.06.2024
+ */
+class StringToUserSortConverterTest {
+    private final StringToUserSortConverter converter = new StringToUserSortConverter();
+
+    @Test
+    void convertTest() {
+        var none = converter.convert("none");
+        Assertions.assertEquals(UserSort.NONE, none);
+
+        var idAsc = converter.convert("id_asc");
+        Assertions.assertEquals(UserSort.ID_ASC, idAsc);
+
+        var idDesc = converter.convert("id_desc");
+        Assertions.assertEquals(UserSort.ID_DESC, idDesc);
+
+        var emailAsc = converter.convert("email_asc");
+        Assertions.assertEquals(UserSort.EMAIL_ASC, emailAsc);
+
+        var emailDesc = converter.convert("email_desc");
+        Assertions.assertEquals(UserSort.EMAIL_DESC, emailDesc);
+
+        var nameAsc = converter.convert("name_asc");
+        Assertions.assertEquals(UserSort.NAME_ASC, nameAsc);
+
+        var nameDesc = converter.convert("name_desc");
+        Assertions.assertEquals(UserSort.NAME_DESC, nameDesc);
+    }
+
+    @Test
+    void convertTestNotValid() {
+        Throwable throwable = Assertions.assertThrows(StringToUserSortConverter.UserSortException.class, () -> converter.convert("asd"));
+
+        Assertions.assertNotNull(throwable);
+        Assertions.assertNotNull(throwable.getMessage());
+        Assertions.assertEquals("ASD", throwable.getMessage());
+    }
+}
